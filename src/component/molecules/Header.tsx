f49bf40cdbb4dd88533abf14f40ef52bf6673447
yunyeop <yykim000922@gmail.com>
2021-07-10 23:44:16 +0900

    feat:header

--- src/component/molecules/Header.tsx
+++ src/component/molecules/Header.tsx
@@ -1,7 +1,51 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Container>
+      <TitleBox>
+        <Circle></Circle>
+        <Title>코각모</Title>
+      </TitleBox>
+      <Nav>
+        <NavItem>Study</NavItem>
+        <NavItem>Login</NavItem>
+        <NavItem>My Page</NavItem>
+      </Nav>
+    </Container>
+  );
 };
 
 export default Header;
+
+const Container = styled.div`
+  display: flex;
+  padding: 3vh 12vw;
+  justify-content: space-between;
+  color: #ffffff;
+  background: #6246e4;
+`;
+
+const TitleBox = styled.div`
+  display: flex;
+`;
+
+const Circle = styled.div`
+  background: #fcbc14;
+  border-radius: 50%;
+  margin-right: 5px;
+  width: 20px;
+  height: 20px;
+`;
+
+const Title = styled.div`
+  line-height: 22px;
+  font-size: 22px;
+`;
+
+const Nav = styled.div``;
+
+const NavItem = styled.span`
+  margin-right: 25px;
+`;
--- src/component/pages/MainPage.tsx
+++ src/component/pages/MainPage.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import {useSelector} from 'react-redux';
 import {RootState} from '../../rootReducer';
+import Header from '../molecules/Header';
 
 const MainPage = () => {
   const _user = useSelector((state: RootState) => state.user);
 
   console.log('user store:', _user);
 
-  return <div>MainPage</div>;
+  return (
+    <div>
+      <Header />
+      <div>MainPage</div>
+    </div>
+  );
 };
 
 export default MainPage;
