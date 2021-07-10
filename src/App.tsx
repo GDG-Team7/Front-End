fa611c109c4f3ebb7be494a64fa925dbe4301e49
yunyeop <yykim000922@gmail.com>
2021-07-11 08:29:27 +0900

    feat:edit router

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import {Route, Switch, BrowserRouter} from 'react-router-dom';
 import CallBack from './component/pages/CallbackPage';
 import Landing from './component/pages/LandingPage';
-import Main from './component/pages/MainPage';
 import RegisterRoomPage from './component/pages/RegisterRoomPage';
 import ProtectedRoute from './routes/ProtectedRoute';
+import Routes from './routes/Routes';
 
 const App = () => {
   return (
@@ -12,12 +12,8 @@ const App = () => {
       <Switch>
         <Route path="/landing" component={Landing} />
         <Route path="/callback" component={CallBack} />
-        <ProtectedRoute
-          path="/register/room"
-          component={RegisterRoomPage}
-          exact={false}
-        />
-        <ProtectedRoute path="/" component={Main} exact={false} />
+        <Route path="/register/room" component={RegisterRoomPage} />
+        <ProtectedRoute path="/" component={Routes} exact={false} />
       </Switch>
     </BrowserRouter>
   );
