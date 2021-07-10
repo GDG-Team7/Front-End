57a3ff2ae83c859828289060af7fc7c0cf52aa22
yunyeop <yykim000922@gmail.com>
2021-07-11 00:30:40 +0900

    feat:불필요 코드 제거

--- src/component/pages/MainPage.tsx
+++ src/component/pages/MainPage.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import {useSelector} from 'react-redux';
 import {RootState} from '../../rootReducer';
-import Header from '../molecules/Header';
 
 const MainPage = () => {
   const _user = useSelector((state: RootState) => state.user);
 
   console.log('user store:', _user);
 
-  return (
-    <div>
-      <Header />
-      <div>MainPage</div>
-    </div>
-  );
+  return <div>MainPage</div>;
 };
 
 export default MainPage;
