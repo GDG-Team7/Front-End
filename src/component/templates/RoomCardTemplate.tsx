e1c4270902ce41e3578308e5550a59dd1e825a24
yunyeop <yykim000922@gmail.com>
2021-07-11 05:19:11 +0900

    feat:add room card

---
+++ src/component/templates/RoomCardTemplate.tsx
@@ -0,0 +1,169 @@
+import React from 'react';
+import styled from 'styled-components';
+import {IMAGES} from '../../constant/Images';
+
+const RoomCardTemplate = () => {
+  return (
+    <CardContainer>
+      <Label>ON</Label>
+      <ProfileImageContainer>
+        <ProfileImage src={IMAGES.room_profile} />
+        <ProfileSubImage src={IMAGES.sub_proflie_1} />
+        <ProfileSubImage2 src={IMAGES.sub_proflie_2} />
+      </ProfileImageContainer>
+      <UserNickName>닉네임이즈</UserNickName>
+      <RoomInfo>
+        <RoomTitle>방이름 입니다.</RoomTitle>
+        <RoomDesc>
+          짧은 소개글 짧은소개글 짧은짧은 소개글 짧은소개글 짧은짧은
+        </RoomDesc>
+      </RoomInfo>
+      <BottomBar>
+        <BottomRoomInfo>
+          <InfoBox>
+            <ClockIcon src={IMAGES.clock} />
+            <Time>1h ago</Time>
+          </InfoBox>
+          <InfoBox>
+            <UsrCountIcon src={IMAGES.user_count_icon} />
+            <PersonnelCount>8/10</PersonnelCount>
+          </InfoBox>
+        </BottomRoomInfo>
+        <PlusButton src={IMAGES.plus_button} />
+      </BottomBar>
+    </CardContainer>
+  );
+};
+
+export default RoomCardTemplate;
+
+const CardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 235px;
+  height: 300px;
+  margin: 0 25px 26px;
+  padding: 10px 15px 21px 15px;
+  border-radius: 15px;
+  box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
+  border: solid 1px #e0e0e0;
+  background-color: #ffffff;
+`;
+
+const Label = styled.div`
+  text-align: center;
+  width: 38px;
+  height: 22px;
+  align-self: flex-end;
+  background: #d7f2e9;
+  padding: 4px 0;
+  border-radius: 5px;
+  background-color: #d7f2e9;
+  font-size: 12px;
+  font-weight: 600;
+  color: #21d59b;
+`;
+
+const ProfileImageContainer = styled.div`
+  position: relative;
+`;
+
+const ProfileSubImage = styled.img`
+  width: 20px;
+  height: 20px;
+  position: absolute;
+  left: 40px;
+  top: 40px;
+`;
+
+const ProfileSubImage2 = styled.img`
+  width: 20px;
+  height: 20px;
+  position: absolute;
+  left: 50px;
+  top: 40px;
+`;
+
+const ProfileImage = styled.img`
+  width: 60px;
+  height: 60px;
+`;
+
+const UserNickName = styled.div`
+  margin-top: 5px;
+  color: #a6adb4;
+  font-weight: normal;
+`;
+
+const RoomInfo = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  margin-top: 20%;
+  text-align: left;
+`;
+
+const RoomTitle = styled.div`
+  font-size: 16px;
+  font-weight: bold;
+  margin-right: auto;
+`;
+
+const RoomDesc = styled.div`
+  display: -webkit-box;
+  white-space: normal;
+  line-height: 20px;
+  height: 40px;
+  overflow: hidden;
+  word-wrap: break-word;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  color: #a6adb4;
+  margin-top: 10px;
+`;
+
+const BottomBar = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  text-align: left;
+  margin-top: 10%;
+  color: #a6adb4;
+`;
+
+const BottomRoomInfo = styled.div``;
+
+const ClockIcon = styled.img`
+  width: 16px;
+  height: 16px;
+  margin-right: 5px;
+`;
+
+const UsrCountIcon = styled.img`
+  width: 16px;
+  height: 16px;
+  margin-right: 5px;
+`;
+
+const InfoBox = styled.div`
+  display: flex;
+  margin-top: 5px;
+`;
+
+const Time = styled.div`
+  font-size: 12px;
+`;
+
+const PersonnelCount = styled.div`
+  font-size: 12px;
+`;
+
+const PlusButton = styled.img`
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  cursor: pointer;
+`;
+
+const PlusImage = styled.div``;
--- src/constant/Images.ts
+++ src/constant/Images.ts
@@ -1,13 +1,21 @@
+import clock from '../assets/images/clock.png';
 import first from '../assets/images/first.png';
 import fourth_1 from '../assets/images/fourth_1.png';
 import fourth_2 from '../assets/images/fourth_2.png';
 import fourth_profile from '../assets/images/fourth_profile.png';
 import github from '../assets/images/github.png';
+import minus_button from '../assets/images/minus_button.png';
+import plus_button from '../assets/images/plus_button.png';
+import room_profile from '../assets/images/room_profile.png';
 import second_1 from '../assets/images/second_1.png';
 import second_2 from '../assets/images/second_2.png';
 import second_3 from '../assets/images/second_3.png';
+import sub_profile_1 from '../assets/images/sub_profile_1.png';
+import sub_profile_2 from '../assets/images/sub_profile_2.png';
+import sub_profile_3 from '../assets/images/sub_profile_3.png';
 import third_1 from '../assets/images/third_1.png';
 import third_2 from '../assets/images/third_2.png';
+import user_count_icon from '../assets/images/user_count_icon.png';
 
 export const IMAGES = {
   github: github,
@@ -20,4 +28,12 @@ export const IMAGES = {
   fourth_1: fourth_1,
   fourth_2: fourth_2,
   fourth_profile: fourth_profile,
+  room_profile: room_profile,
+  sub_proflie_1: sub_profile_1,
+  sub_proflie_2: sub_profile_2,
+  sub_proflie_3: sub_profile_3,
+  plus_button: plus_button,
+  minus_button: minus_button,
+  clock: clock,
+  user_count_icon: user_count_icon,
 };
